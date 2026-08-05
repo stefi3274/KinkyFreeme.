0ef548c58c5ccf1ca0c162d99418e6eb5f56771f
SteFi <stefi3274@gmail.com>
2026-08-05 14:50:32 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-//  KINKY, FREE ME — Supabase Configuration
-//  Ne pas partager ce fichier publiquement
-// ============================================================
-
-const SUPABASE_URL  = 'https://mvctupmvwhfhrgaupboi.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12Y3R1cG12d2hmaHJnYXVwYm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMDE2NzMsImV4cCI6MjA5NTU3NzY3M30.uBNLDow2KWW4a8AxjTNlrXzrKPzmU-h_mTGizkHp2Uk';
